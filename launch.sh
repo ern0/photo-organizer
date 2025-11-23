08b3acbd20f1382daf2bf510558a25f513d31e87
ern0 <ern0@linkbroker.hu>
2025-11-23 22:23:39 +0100

    add some exception

--- launch.sh
+++ launch.sh
@@ -23,5 +23,11 @@ fi
 	--source /media/storage/home/ern0/pixx \
 	--target /media/storage/home/ern0/orgpixx \
 	--ignore-list \
-		pix-select \
-		_funi _mixed _tibi_canon_a100 agi_nandipix anim levivasut x y
+		/media/storage/home/ern0/pixx/pix-select \
+		/media/storage/home/ern0/pixx/pix1/0_misc/_funi \
+		/media/storage/home/ern0/pixx/pix1/0_misc/_mixed \
+		/media/storage/home/ern0/pixx/pix1/0_misc/_tibi_canon_a100 \
+		/media/storage/home/ern0/pixx/pix1/0_misc/anim \
+		/media/storage/home/ern0/pixx/pix1/0_misc/levivasut \
+		/media/storage/home/ern0/pixx/pix1/0_misc/x \
+		/media/storage/home/ern0/pixx/pix1/0_misc/y
